3775865d0ee2966500ad240ee594fdfb72d6c1a1
Theoden Melgar <theodenmelgar@gmail.com>
2025-01-28 14:28:37 -0800

    initial commit, set up chakra

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "@chakra-ui/react"
+import { HStack } from "@chakra-ui/react"
+
+function App() {
+
+  return (
+    <>
+      <HStack>
+      <Button bg='red'>Click me</Button>
+      <Button>Click me</Button>
+      </HStack>
+    </>
+  )
+}
+
+export default App
